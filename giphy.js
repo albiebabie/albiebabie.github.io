adbe49dfdb29f4ef13ad43638b8508c8994f8ad3
alban <alban.allen@endclothing.com>
2018-08-23 13:01:37 +0100

    can make a successful api call to get 10 trending gifs

--- giphy.js
+++ giphy.js
@@ -1,3 +1,14 @@
 var GIPHY_KEY = config.GIPHY_KEY;
-var xhr = $.get("http://api.giphy.com/v1/gifs/trending?api_key=" + GIPHY_KEY + "&limit=10"); 
-            xhr.done(function(data) { console.log("hello your data is here...", data); });
+var xhr = $.get("http://api.giphy.com/v1/gifs/trending?api_key=" + GIPHY_KEY + "&limit=10");
+
+xhr.done(function(data) {
+  data.data.forEach((gif, i) => {
+    var li = document.createElement("LI");
+    var a = document.createElement("A")
+    a.textContent = ++i;
+    a.href = gif.images.fixed_height.url;
+    li.appendChild(a);
+    document.getElementById("js-gifs-list").appendChild(li);
+  });
+});
+
