693f40ddfe33f31a0d063f4282814e3628cf6d85
alban <alban.allen@endclothing.com>
2018-08-23 14:27:09 +0100

    now generates gifs on the page

--- giphy.js
+++ giphy.js
@@ -4,10 +4,10 @@ var xhr = $.get("http://api.giphy.com/v1/gifs/trending?api_key=" + GIPHY_KEY + "
 xhr.done(function(data) {
   data.data.forEach((gif, i) => {
     var li = document.createElement("LI");
-    var a = document.createElement("A")
-    a.textContent = ++i;
-    a.href = gif.images.fixed_height.url;
-    li.appendChild(a);
+    var img = document.createElement("IMG")
+    img.textContent = ++i;
+    img.src = gif.images.fixed_height.url;
+    li.appendChild(img);
     document.getElementById("js-gifs-list").appendChild(li);
   });
 });
